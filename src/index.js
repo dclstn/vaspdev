1cf1c46b021b212439ff751d88256ba487b512ea
Vasp1 <43322006+Vasp1@users.noreply.github.com>
2020-08-18 19:20:33 +0100

    😃 Fixing imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
 
-import Root from './root';
+import Root from './root.js';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 
